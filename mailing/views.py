b602046a88d9d4fb11514204276d33b92ee87324
Galina Lukina <lukina.galina11@gmail.com>
2024-12-10 10:37:41 +0400

    add CRUD for Recipient

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
-# Create your views here.
+from mailing.models import Recipient
+
+
+class RecipientListView(ListView):
+    model = Recipient
+
+
+class RecipientDetailsView(DetailView):
+    model = Recipient
+
+
+class RecipientCreateView(CreateView):
+    model = Recipient
+    form_class = RecipientForm
+    success_url = reverse_lazy("mailing:recipient_list")
+
+
+class RecipientUpdateView(UpdateView):
+    model = Recipient
+    form_class = RecipientForm
+    success_url = reverse_lazy("mailing:recipient_list")
+
+
+class RecipientDeleteView(DeleteView):
+    model = Recipient
+    success_url = reverse_lazy("mailing:recipient_list")
